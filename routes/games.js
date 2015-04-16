82aafc43d672ad15e9da9838fecc615d3a46f45e
Henrik Grankvist <henrik.grankvist@gmail.com>
2015-04-16 07:50:54 +0200

    First version that works a little better, it is possible to take a position now.
    But there is no validation!!!!!!!!!

--- routes/games.js
+++ routes/games.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,19 +8,17 @@ var logger = log4js.getLogger();
 
 var bus = require('../bus');
 
-var ObjectID = require('mongodb').ObjectID
+var ObjectID = require('mongodb').ObjectID;
 
 var stream = 'game';
 
-
-//Todo do some initialization from the db. find a current game.
+var m = require("monet");
 
 function theGame(evt) {
     logger.debug("here we go: " + JSON.stringify(evt));
 }
 
 bus.on('event', theGame);
-
 
 /**
  *
@@ -27,11 +27,11 @@ bus.on('event', theGame);
  * @returns {null|*}
  */
 function getGame(db, deliver) {
-    return db.collection('game').find().toArray(function (err, items) {
+    return db.collection('game').find({name: "theGame1"}).toArray((err, items) => {
         if (items.length == 0) {
-            deliver();
+            deliver(m.Maybe.None());
         } else {
-            deliver(items[0]);
+            deliver(m.Maybe.Some(items[0]));
         }
     });
 }
@@ -39,108 +39,134 @@ function getGame(db, deliver) {
 /*
  * GET current ongoing game.
  */
-router.get('/current', function (req, res) {
-    getGame(req.db, function (items) {
-        res.json(items);
-    });
-});
-
+router.get('/current', (req, res) => getGame(req.db, (items) => res.json(items)));
 
 /*
  * POST to adduser.
  */
-router.post('/adduser', function (req, res) {
+router.post('/adduser', (req, res) => {
     var db = req.db;
-    db.collection('userlist').insert(req.body, function (err, result) {
-        res.send(
-            (err === null) ? {msg: ''} : {msg: err}
-        );
-    });
+    db.collection('userlist').insert(req.body, (err, result) => res.send((err === null) ? {msg: ''} : {msg: err}));
 });
 
 function fireGameCreated(es, callback) {
-    es.getEventStream(stream,
-        function (err, stream) {
-            stream.addEvent(
-                {
-                    createGame: {
-                        gameId: '1234'
-                    }
-                });
+    es.getEventStream(stream, (err, stream) => {
+        stream.addEvent(
+            {
+                createGame: {
+                    gameId: '1234'
+                }
+            });
 
-            stream.commit();
-        });
+        stream.commit();
+    });
 }
 
 /*
- * POST to take position.
+ * POST to create the game, it is only possible to create a game if there is none running..
  */
-router.post('/createGame', function (req, res) {
+router.post('/createGame', (req, res) => {
     logger.debug("Create a new game");
 
     var es = req.es;
     var db = req.db;
 
-    var game = getGame(req.db, function (game_1) {
+    var game = getGame(req.db, (game_1) => {
         var id = new ObjectID();
 
-        if (game_1 !== undefined && game_1.winner === "") {
+        if (game_1.isSome() && game_1.some().winner === "") {
             logger.debug("Nope the previous game is still in progress...");
             res.json({
                 gameId: game_1.currentId,
-                msg: "Game already started"});
+                msg: "Game already started"
+            });
 
         } else {
             logger.debug("Create a new one on this mf: " + JSON.stringify(game_1));
-            db.collection('game').update({name: 'theGame'},
-                {
-                    name: "theGame", //Very static, we'll try to find another way later on.
-                    currentId: id.toHexString(),
-                    timeStarted: new Date().toISOString(),
-                    timeEnded: "",
-                    team_one: {},
-                    "team_two": {},
-                    "score": {
-                        "team_one": 0,
-                        "team_two": 0
-                    },
-                    "winner": ""
+            var d = new Date().toISOString();
+            var g = {
+                name: "theGame1", //Very static, we'll try to find another way later on.
+                currentId: id.toHexString(),
+                timeStarted: d,
+                timeEnded: "",
+                team_one: {},
+                team_two: {},
+                score: {
+                    team_one: 0,
+                    team_two: 0
                 },
-                upsert=true);
-            res.json({newGameId: id.toHexString()}); //Todo better
-        }
+                winner: ""
+            };
 
+            db.collection('game').update({name: "theGame1"}, g, (err) => {
+                logger.error(err);
+                if (err) throw err;
+            });
+
+            //We respond to the client once the event has been committed on the stream.
+            es.getEventStream(stream,
+                function (err, stream) {
+                    stream.addEvent(
+                        {
+                            gameCreated: {
+                                gameId: id.toHexString(),
+                                timeStarted: d
+                            }
+                        });
+                    stream.commit();
+                    res.json({game: g});
+                });
+        }
     });
 });
 
 
+function updatePosition(team, position, player, game) {
+    var g = JSON.parse(JSON.stringify(game));
+    g[team][position] = player;
+    return g;
+}
+
 /*
  * POST to take position.
  */
-router.post('/takePosition/:gameId/:team/:position/:player', function (req, res) {
+router.post('/takePosition/:gameId/:team/:position/:player', (req, res) => {
     var es = req.es;
-    logger.debug("position: " + req.params.team);
+    var db = req.db;
 
-    //Todo change the name of the stream id.
+    getGame(req.db, (game) => {
+        if (game.isSome()) {
 
-    es.getEventStream(stream,
-        function (err, stream) {
-            stream.addEvent(
-                {
-                    positionTaken: {
-                        gameId: req.params.gameId,
-                        team: req.params.team,
-                        position: req.params.position,
-                        player: req.params.player
-                    }
+            var t = req.params.team;
+            var g = updatePosition(t, req.params.position, req.params.player, game.some());
+
+            var new_team = {};
+            new_team[t] = g[t];
+
+            db.collection('game').update({name: "theGame1"}, {$set: new_team}, (err) => {
+                logger.debug("Db updated! Or not! ?: " + err);
+
+                if (err) throw err;
+            });
+
+            es.getEventStream(stream, (err, stream) => {
+                    stream.addEvent(
+                        {
+                            positionTaken: {
+                                gameId: req.params.gameId,
+                                team: req.params.team,
+                                position: req.params.position,
+                                player: req.params.player
+                            }
+                        });
+
+                    stream.commit();
+                    res.send((err === null) ? {game: g} : {game: err});
                 });
-
-            stream.commit();
-
-            res.send(
-                (err === null) ? {msg: ''} : {msg: err}
-            );
-        });
+        } else {
+            res.send("No game running!");
+        }
+    });
 });
 
 
